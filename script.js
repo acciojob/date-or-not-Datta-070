866a79ff56f3a2ec0757abe10b26d2525228be3d
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-20 12:56:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var isDate = function (input) {
   //   write your code here
+	if (input instanceof Date) {
+    return true;
+  }
+
+  // Try parsing the input as a date
+  const parsedDate = new Date(input);
+
+  // Check if the parsed date is a valid date and not NaN
+  return !isNaN(parsedDate.getTime());
 };
 
 // Do not change the code below.
